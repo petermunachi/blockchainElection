23009acacbe6770d273519054ecc08cb33765a96
petermunachi <alimunachipeter@gmail.com>
2021-09-02 12:47:16 +0100

    changing src to docs

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,49 @@
+
+
+const fs = require('fs');
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const BSCSCANAPIKEY = process.env.BSCSCANAPIKEY;
+
+
+
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
+    testnet: {
+      provider: () => new HDWalletProvider(
+        mnemonic, 
+        `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(
+        mnemonic, 
+        `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
-};
+}
